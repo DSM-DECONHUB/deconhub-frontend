fb0d91a0851d72f585cccda164f7c36e300a680c
jaemin <sonjm3518@dsm.hs.kr>
2022-11-16 22:21:01 +0900

    feat: apis

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,9 +1,43 @@
-import {instance} from "./instance";
+import {BaseUrl} from "./instance";
+import axios from "axios";
 
-export const getContestList = async () => {
-    return await instance.get().data;
+export const getList = async() => {
+    let data
+    await axios({
+        url:BaseUrl+"/contest/list",
+        method:'get'
+    }).then((res)=>data=res)
+
+    return data.data.list;
 }
 
-export const getQuA = async () => {
-    return await instance.get().data;
+export const getQnAList = async() => {
+    let data
+    await axios({
+        url:BaseUrl+"/qna/list",
+        method:'get'
+    }).then((res)=>data=res)
+
+    return data.data.list;
+}
+
+export const deleteContest = async(id) => {
+    await axios({
+        url:BaseUrl+"/contest/delete"+ id,
+        method:'delete'
+    })
+}
+
+export const postContest = async() => {
+    await axios({
+        url: BaseUrl+"/contest/create",
+        method:'post'
+    })
+}
+
+export const postQnA = async(id) => {
+    await axios({
+        url: BaseUrl+"qna/create",
+        method:'post'
+    })
 }
